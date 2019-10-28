fd5eb0a94a93a3a1045b9e58300824c1a2a917b8
sajith711 <42170577+sajith711@users.noreply.github.com>
2019-10-28 17:19:16 +0530

    Added Controller Advice annotation

--- src/main/java/org/yantranet/etas/exception/RestExceptionHandler.java
+++ src/main/java/org/yantranet/etas/exception/RestExceptionHandler.java
@@ -15,8 +15,10 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler{
 	private static final String ERROR = "errors";
 	private static final String STATUS = "status";
