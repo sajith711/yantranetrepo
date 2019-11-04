9409f89d72204f78b0e46a67f580e62b733647b3
sajith711 <42170577+sajith711@users.noreply.github.com>
2019-11-04 17:21:57 +0530

    added import statements

--- src/main/java/org/yantranet/etas/model/CabRequest.java
+++ src/main/java/org/yantranet/etas/model/CabRequest.java
@@ -6,6 +6,8 @@ import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Entity(name = "cabrequest")
 @Table(name = "cabrequest")
